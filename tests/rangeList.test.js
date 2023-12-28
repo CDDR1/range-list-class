99273b49e18af5b3b58b6cd9cc3636ed5a649b2d
Carlos Duque <carlos.d.duque.r@gmail.com>
2023-12-28 12:21:15 -0500

    Added test script and first unit test.

--- tests/rangeList.test.js
+++ tests/rangeList.test.js
@@ -1,2 +1,27 @@
 import RangeList from "../rangeList.js";
 
+describe("RangeList", () => {
+  let rangeList = null;
+
+  beforeEach(() => {
+    rangeList = new RangeList();
+  });
+
+  describe("add method", () => {
+    test("Adding non-overlapping ranges", () => {
+      rangeList.add([4, 9]);
+      rangeList.add([0, 2]);
+      rangeList.add([12, 15]);
+
+      expect(rangeList.rangeList).toStrictEqual([
+        [0, 2],
+        [4, 9],
+        [12, 15],
+      ]);
+    });
+  });
+
+  describe("remove method", () => {});
+
+  describe("toString method", () => {});
+});
